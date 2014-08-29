6f7ad04241d591e8031045928334befe0870d20b
bueltge <frank@bueltge.de>
2014-08-29 16:08:20 +0200

    add hint

--- remove-comments-absolute.php
+++ remove-comments-absolute.php
@@ -248,7 +248,7 @@ if ( ! class_exists( 'Remove_Comments_Absolute' ) ) {
 					$( '.welcome-comments' ).remove();
 					// 'Right Now' dashboard widget
 					$( 'div.table_discussion:first' ).remove();
-					// 'Right Now' dashbaord widget since WP version 3.8
+					// 'Right Now' dashbaord widget since WP version 3.8, second ID since WP 4.0
 					$( 'div#dash-right-now, #dashboard_right_now' ).find( '.comment-count').remove();
 					// 'Activity' dashboard widget, since WP version 3.8
 					$( 'div#dashboard_activity').find( '#latest-comments' ).remove();
