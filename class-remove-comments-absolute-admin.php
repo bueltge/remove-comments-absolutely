7c9c6010d196e9e280a3cb830d1431e7fdb169b1
Barry Ceelen <barry@multipop.org>
2015-11-16 18:17:59 +0100

    Add separate classes for public and admin

---
+++ class-remove-comments-absolute-admin.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Remove Comments Absolutely
+ *
+ * @package   Remove_Comments_Absolutely
+ * @author    Frank Bültge
+ * @license   GPL-2.0+
+ * @link      https://github.com/bueltge/Remove-Comments-Absolutely/
+ * @copyright 2015 Frank Bültge
+ */
+
+/**
+ * Admin plugin class.
+ *
+ * @package   Remove_Comments_Absolutely
+ * @author    Frank Bültge
+ */
+class Remove_Comments_Absolute_Admin {
+
+	/**
+	 * Class object.
+	 *
+	 * @var null
+	 */
+	static private $classobj;
+
+	/**
+	 * Get an instance of this class.
+	 *
+	 * @access public
+	 * @since  0.0.1
+	 * @return null|Remove_Comments_Absolute $classobj object
+	 */
+	public static function get_object() {
+
+		if ( NULL === self::$classobj ) {
+			self::$classobj = new self;
+		}
+
+		return self::$classobj;
+	}
+}
